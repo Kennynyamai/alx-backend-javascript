216f91136b98c58b252b762ff8028dfc5f999f1b
kennynyamai <kennynamai73@gmail.com>
2023-08-22 20:30:15 +0300

    nodejs

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -4,14 +4,4 @@ export default class ClassRoom {
     constructor(maxStudentsSize) {
       this._maxStudentsSize = maxStudentsSize;
     }
-  
-    set maxStudentsSize(newMaxStudentsSize) {
-      if (typeof newMaxStudentsSize === 'number') {
-        this._maxStudentsSize = newMaxStudentsSize;
-      }
-    }
-  
-    get maxStudentsSize() {
-      return this._maxStudentsSize;
-    }
   }
